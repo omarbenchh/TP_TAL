a818c90af27eee8598b1a52ae2d39ce18360ec24
Omar BENCHHIBA <omar@benchhiba.com>
2015-10-14 17:51:42 +0200

    Calcul de précision et rappel

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from collections import Counter
 import re
 import csv
@@ -12,8 +13,6 @@ parties = re.split(myRegex, textInput)
 
 myRegexChap = re.compile('\n\n \n\n[IXVLMC]*[IXVLMC]\n\n')
 chapitres = []
-
-
 
 for count, element in enumerate(parties):
     chapitres.append(re.split(myRegexChap, element))
@@ -41,43 +40,62 @@ for partie in paragraphes:
 test = []
 mywords = []
 
-for index1, part in enumerate(paragrapheChapitre):
-    if index1 != 0:
-        with open('Segments\\partie' + str(index1) + '.csv', 'wb') as csvfile:
-            fieldnames = ['Mots', 'Partie', 'Chapitre', 'Paragraphe', 'Phrase']
-            monFichierCSV = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            monFichierCSV.writeheader()
-            for index2, chap in enumerate(part):
-                for index3, parag in enumerate(chap):
-                    for index4, phra in enumerate(parag):
-                        test.append(paragrapheChapitre[index1][index2][index3][index4].words)
-                        for index5, mots in enumerate(test):
-                            for index6, tmp in enumerate(mots):
-                                mywords.append(str(tmp.encode('UTF-8')))
-                                monFichierCSV.writerow({'Mots': str(tmp.encode('UTF-8')), 'Partie': index1,
-                                                        'Chapitre': index2 + 1, 'Paragraphe': index3 + 1,
-                                                        'Phrase': index4 + 1})
-                        test = []
-        counts = Counter(mywords)
-        with open('NoRep\\partie' + str(index1) + '_noRepeat.csv', 'wb') as csvfile:
-            fieldnames = ['Mots', 'Effectif', 'Longueur', 'Partie']
-            monFichierCSV2 = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            monFichierCSV2.writeheader()
-            for singleWord in counts:
-                monFichierCSV2.writerow({'Mots': str(singleWord),
-                                         'Effectif': counts[singleWord],
-                                         'Longueur': len(singleWord), 'Partie': index1})
-        mywords = []
+# for index1, part in enumerate(paragrapheChapitre):
+#     if index1 != 0:
+#         with open('Segments\\partie' + str(index1) + '.csv', 'wb') as csvfile:
+#             fieldnames = ['Mots', 'Partie', 'Chapitre', 'Paragraphe', 'Phrase']
+#             monFichierCSV = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#             monFichierCSV.writeheader()
+#             for index2, chap in enumerate(part):
+#                 for index3, parag in enumerate(chap):
+#                     for index4, phra in enumerate(parag):
+#                         test.append(paragrapheChapitre[index1][index2][index3][index4].words)
+#                         for index5, mots in enumerate(test):
+#                             for index6, tmp in enumerate(mots):
+#                                 mywords.append(str(tmp.encode('UTF-8')))
+#                                 monFichierCSV.writerow({'Mots': str(tmp.encode('UTF-8')), 'Partie': index1,
+#                                                         'Chapitre': index2 + 1, 'Paragraphe': index3 + 1,
+#                                                         'Phrase': index4 + 1})
+#                         test = []
+#         counts = Counter(mywords)
+#         with open('NoRep\\partie' + str(index1) + '_noRepeat.csv', 'wb') as csvfile:
+#             fieldnames = ['Mots', 'Effectif', 'Longueur', 'Partie']
+#             monFichierCSV2 = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#             monFichierCSV2.writeheader()
+#             for singleWord in counts:
+#                 monFichierCSV2.writerow({'Mots': str(singleWord),
+#                                          'Effectif': counts[singleWord],
+#                                          'Longueur': len(singleWord), 'Partie': index1})
+#         mywords = []
 
-myRegexNoms = re.compile('[^\.\-\xe0!?] [A-Z][a-z]+')
+myRegexNoms = re.compile('[^\.\-\xe0!?] [A-Z][a-z]+ [A-Z][a-z]+|[^\.\-\xe0!?] [A-Z][a-z]+')
 noms = myRegexNoms.findall(textInput)
 noms2 = []
 for nom in noms:
-    noms2.append(nom[2:])
-noms = set(noms2)
+    if nom[2:] not in noms2 and len(nom[2:]) > 2:
+        noms2.append(nom[2:])
+
 with open('listeNoms.csv', 'wb') as csvfile:
     fieldnames = ['Noms']
     monFichierCSV0 = csv.DictWriter(csvfile, fieldnames=fieldnames)
     monFichierCSV0.writeheader()
-    for nom in noms:
+    for nom in noms2:
         monFichierCSV0.writerow({'Noms': str(nom)})
+
+personnages = []
+with open('personnage.csv') as csvfile:
+    spamreader = csv.reader(csvfile)
+    for row in spamreader:
+        personnages.append(row[0])
+p= set(personnages)
+t = set(noms2)
+
+vp = len(p.intersection(t))
+fn = len(p.difference(t))
+fp = len(t.difference(p))
+
+rappel = (float(vp) / (vp+fn))
+precision = (float(vp)/(vp+fp))
+
+print('Rappel :', rappel)
+print('Précision :', precision)
